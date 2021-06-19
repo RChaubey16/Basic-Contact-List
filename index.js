9d27e5a6b444833f4dc77be918dc2c0fdd188e0a
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-06-19 16:39:48 +0530

    Sixth Commit: Added the MongoDb and established connecetion with the db

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 7000;
+// fetching the connected database
+const db = require("./config/mongoose");
+
 // setting the view engine as ejs and also the views folder
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// setting up the middleware, so that the object body is created in the request made by client
 app.use(express.urlencoded());
 
 app.use("/", require("./routes"));
