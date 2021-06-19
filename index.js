3ac5dd891dd343fe8a6417182e78eef632fcc8f3
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-06-19 15:14:46 +0530

    Second Commit: Created MVC directory and installed ejs as the view engine

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 7000;
+// setting the view engine as ejs and also the views folder
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    title: "My Contact List",
+  });
+});
+
+// Running the server
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server on port: ", port);
+    return;
+  }
+
+  console.log("Server is up and running on port: ", port);
+});
